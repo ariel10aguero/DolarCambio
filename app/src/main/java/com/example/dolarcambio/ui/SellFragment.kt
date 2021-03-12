c74945335fa676011e82e1f755c2cdfc2bb62f0e
Alejandro <ariel10aguero@gmail.com>
2021-03-12 11:50:14 -0300

    Calendar on sellFragment

--- app/src/main/java/com/example/dolarcambio/ui/SellFragment.kt
+++ app/src/main/java/com/example/dolarcambio/ui/SellFragment.kt
@@ -1,21 +1,25 @@
 package com.example.dolarcambio.ui
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import androidx.navigation.fragment.findNavController
+import com.example.dolarcambio.CalendarUtils
 import com.example.dolarcambio.R
 import com.example.dolarcambio.closeKeyboard
 import com.example.dolarcambio.databinding.FragmentBuyBinding
 import com.example.dolarcambio.databinding.FragmentSellBinding
 
 
-class SellFragment : Fragment() {
+class SellFragment : Fragment(), DatePickerDialog.OnDateSetListener {
 
     private var _binding: FragmentSellBinding? = null
     private val binding get() = _binding!!
+    private val calendarUtils = CalendarUtils()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,10 +43,24 @@ class SellFragment : Fragment() {
             closeKeyboard(it)
             findNavController().navigate(R.id.action_sellFragment_to_chooseFragment)
         }
+
+        binding.sellDateBtn.text = calendarUtils.setBtnDate()
+        binding.sellDateBtn.setOnClickListener {
+            DatePickerDialog(requireContext(),this, calendarUtils.year, calendarUtils.month, calendarUtils.day).show()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        calendarUtils.year = year
+        calendarUtils.month = month
+        calendarUtils.day = dayOfMonth
+        val realMonth = month + 1
+
+        binding.sellDateBtn.text = "$dayOfMonth/$realMonth/$year"
     }
 }
